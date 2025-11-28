c542154a7d086e679d83b247b88072e11e7e8fb4
20721 <11335150@dcsh.tp.edu.tw>
2025-11-28 12:04:20 +0800

    連結的?異動腳本?(通用去改)

--- default/urls.py
+++ default/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from views import poll_list
+from .views import poll_list, PollList, PollView, PollVote
 
 urlpatterns = [
   path("", poll_list),#專案的處理規則都要列在專案中(poll/urls)
-
+  path("list", PollList.as_view(), name = 'poll_list'),
+  #路徑可以埋參數，統一在同一條路徑
+  path("<int:pk>/", PollView.as_view(), name = 'poll_view'),
+  #如果是整數，擷取，撈參數
+  #poll_list中的{% url 'poll_view' poll.id %}方便更改<int:pk>/
+  path('<int:pk>/vote/', PollVote.as_view(), name = 'poll_vote'),
 ]
--- default/views.py
+++ default/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
-from models import poll
+from .models import poll,option
+from django.views.generic import ListView, DetailView,RedirectView
 
 # Create your views here.
 def poll_list(req):#接收django 傳給你的使用者請求 名稱(req)
-    polls = poll.objects.all()
+    polls = poll.objects.all()#poll的所有東西回傳，收在polls
     return render(req, "default/list.html", {'poll_list' : polls,'msg':'Hello!'})
-    #回報 繪製(運算) (參數,位置,{可用變數:變數(名稱可不同),msg})
+    #回報 繪製(運算) (參數,位置,{可用變數:變數(名稱可不同),msg})，字典 對應關係
+
+class PollList(ListView):
+    model = poll
+    #通用去改(不用字典)
+    #應用程式名稱/資料模型_list.html
+    #default/poll_list.html
+    #預設名稱 葉面範本黨名
+
+class PollView(DetailView):
+    model = poll
+    #default/poll_detail.html
+
+    def get_context_data(self, **kwargs):
+                        #自己,關鍵藏數
+        ctx = super().get_context_data(**kwargs)#字典(detailview 原本要傳的)
+        ctx['option_list'] = option.objects.filter(poll_id = self.object.id)
+        return ctx
+    
+class PollVote(RedirectView):
+                #會跳轉回來的
+    #model = poll
+    pass
